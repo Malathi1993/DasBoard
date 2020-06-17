d297e636f9eb7322f87623ac1ccf2a9c40f23775
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-06-17 13:07:53 +0530

    Add

--- app/src/main/java/com/example/dasboard/HistoryActivity.java
+++ app/src/main/java/com/example/dasboard/HistoryActivity.java
@@ -2,13 +2,22 @@ package com.example.dasboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends Activity {
+
+    ListView simpleList;
+    String List[] = {"Rent", "Return", "NearBy", "Histry"};
+    int img [] = {R.drawable.phone, R.drawable.phone, R.drawable.phone, R.drawable.phone};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), List, img);
+        simpleList.setAdapter(customAdapter);
     }
 }
